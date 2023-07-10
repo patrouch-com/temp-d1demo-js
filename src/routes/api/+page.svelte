06cb608063ea74237edba57747658d9c58917cd6
Chef <cheftech@patraldo.dev>
2023-07-09 18:13:51 -0600

    first commit

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Bienvenid@s a Mexican Bold</h1>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+h1 {
+font-family: 'Special Elite', cursive;
+font-size: 3.0rem;
+padding: 20px 20px 60px;
+max-width: 680px;
+margin: 0 auto;
+}
+
+</style>
